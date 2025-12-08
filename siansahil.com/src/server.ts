a40808a25dbb8f1b47d552c3961ed090d2fa475e
Sahil Sian <sh864246@dal.ca>
2025-12-07 16:55:36 -0800

    added server.ts entry point

---
+++ siansahil.com/src/server.ts
@@ -0,0 +1,8 @@
+// src/server.ts
+import handler, { createServerEntry } from '@tanstack/react-start/server-entry'
+
+export default createServerEntry({
+    fetch(request) {
+        return handler.fetch(request)
+    },
+})
