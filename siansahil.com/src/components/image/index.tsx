86353b1960d95003f81a3f29810b552e1c03ddd7
Sahil Sian <sh864246@dal.ca>
2025-12-07 23:12:16 -0400

    check for images to include full url

--- siansahil.com/src/components/image/index.tsx
+++ siansahil.com/src/components/image/index.tsx
@@ -1,30 +1,37 @@
-export interface StrapiImage {
-    url: string;
-    width: number;
-    height: number;
-    alternativeText?: string | null;
-    caption?: string | null;
-    name: string;
-}
+    export interface StrapiImage {
+        url: string;
+        width: number;
+        height: number;
+        alternativeText?: string | null;
+        caption?: string | null;
+        name: string;
+    }
 
-interface ImageProps {
-    image: StrapiImage;
-    className?: string;
-    objectFit?: "cover" | "contain" | "fill" | "none" | "scale-down";
-    objectPosition?: "top" | "right" | "bottom" | "left "
-}
+    interface ImageProps {
+        image: StrapiImage;
+        className?: string;
+        objectFit?: "cover" | "contain" | "fill" | "none" | "scale-down";
+        objectPosition?: "top" | "right" | "bottom" | "left "
+    }
 
-const toImageProps = (img:StrapiImage) => ({
-    src: import.meta.env.VITE_STRAPI_ROOT + img.url,
-    width: img.width,
-    height: img.height,
-    alt: img.alternativeText || img.caption || img.name,
-});
+    const toImageProps = (img: StrapiImage) => {
+        const src = img.url.startsWith('http')
+            ? img.url
+            : `${import.meta.env.VITE_STRAPI_ROOT}${img.url}`;
+
+        return {
+            src,
+            width: img.width,
+            height: img.height,
+            alt: img.alternativeText || img.caption || img.name,
+        };
+    };
 
 
-const Image = ({ image, objectFit="cover", objectPosition="top", className = "IMG" }:ImageProps) => {
-    const props = toImageProps(image);
-    return <img className={className} style={{objectFit: objectFit, objectPosition: objectPosition}} {...props} />;
-};
+    const Image = ({ image, objectFit="cover", objectPosition="top", className = "IMG" }:ImageProps) => {
+        const props = toImageProps(image);
 
-export default Image;
+        return <img className={className} style={{objectFit: objectFit, objectPosition: objectPosition}} {...props} />;
+    };
+
+    export default Image;
